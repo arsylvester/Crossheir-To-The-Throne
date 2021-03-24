2eba5e1f48955f5871db48fd30f67be15fa57d35
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-03-24 16:01:25 -0500

    Test sound set up
    Wwise integration looks to be working

---
+++ Assets/Scripts/TestSound.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            AkSoundEngine.PostEvent("TestEvent", gameObject);
+        }
+    }
+}
