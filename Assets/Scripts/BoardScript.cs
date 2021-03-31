16bc7fdb4318256cb0154769cf135f60bcfd541f
Noah Sims <noahsims24@gmail.com>
2021-03-31 00:23:24 -0500

    ScoreBoard Scripting Done

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -11,6 +11,42 @@ public class BoardScript : MonoBehaviour
 
     public void drawStats()
     {
-        BoardTimer.GetComponent<TextMeshPro>().SetText(TimeMaster.timeToString(TimeMaster.currentTime));
+        BoardTimer.GetComponent<TextMeshPro>().SetText(TimeMaster.timerText);
+        // This is really dumb, but don't worry about it
+        BoardRecords.GetComponent<TextMeshPro>().SetText(
+            "__Course Records__\n" + 
+            "Dev Time: 09:18.32\n" + 
+            "Gold: 02:34.44\n" + 
+            "Silver: 09:22.88\n" + 
+            "Bronze: 10:21.32\n" + 
+            "\nYour Best Time:\n" +
+            TimeMaster.timeToString(PlayerPrefs.GetFloat("HighScore", 9999f)));
+
+        BoardStats.GetComponent<TextMeshPro>().SetText(
+            "__Stats__\n" + 
+            "Total Shots: " + WeaponManager.shotsTaken.ToString() + "\n" + 
+            "Accuracy: " + (int)(((float)WeaponManager.shotsHit / (float)WeaponManager.shotsTaken) * 100) + "%\n" + 
+            "Max KillStreak: " + WeaponManager.maxKillstreak.ToString() + "\n" +
+            "Max ShotStreak: " + WeaponManager.maxShotstreak.ToString());
+        //((int)(time * 100)).ToString("00")
     }
 }
+
+/* BOARD RECORDS
+__Course Records__
+Dev Time:	09:18.32
+Gold:		02:34.44
+Silver:		09:22.88
+Bronze:	10:21.32
+
+Your Best Time:
+00:00.00
+ */
+
+/* BOARD STATS
+__Stats__
+Total Shots: XX
+Accuracy: XX%   
+Max KillStreak: XX
+Max ShotStreak: XX
+ */
