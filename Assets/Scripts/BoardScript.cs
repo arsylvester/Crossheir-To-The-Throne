7bf69761dc6ef1a8ba8c44298aac8cba0588c1da
Henry <htwerner01@gmail.com>
2021-03-30 22:33:30 -0500

    Auto stash before merge of "Player-Control" and "origin/Player-Control"

---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BoardScript : MonoBehaviour
+{
+    [SerializeField] GameObject BoardTimer;
+    [SerializeField] GameObject BoardRecords;
+    [SerializeField] GameObject BoardStats;
+
+    public void drawStats()
+    {
+        BoardTimer.GetComponent<TextMeshPro>().SetText(TimeMaster.timeToString());
+    }
+}
